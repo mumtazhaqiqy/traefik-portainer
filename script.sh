8fc800473ec601e07aa953442e1d3a26daf2ff20
Mumtaz Moon <37664171+mumtazhaqiqy@users.noreply.github.com>
2024-05-25 16:05:21 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-# Create directories and files for Traefik
-mkdir -p /mnt/data/traefik
-touch /mnt/data/traefik/acme.json
-chmod 600 /mnt/data/traefik/acme.json
+# Update and upgrade the system
+sudo apt-get update
+sudo apt-get upgrade -y
 
-# Create directory for Portainer
-mkdir -p /mnt/data/portainer
+# Install Docker
+curl -fsSL https://get.docker.com | sudo bash
 
 # Initialize Docker Swarm
-docker swarm init
+sudo docker swarm init
 
 # Create an overlay network named traefik-public
-docker network create --driver overlay traefik-public
+sudo docker network create --driver overlay traefik-public
+
+# Create directories and files for Traefik
+sudo mkdir -p /mnt/data/traefik
+sudo touch /mnt/data/traefik/acme.json
+sudo chmod 600 /mnt/data/traefik/acme.json
+
+# Create directory for Portainer
+sudo mkdir -p /mnt/data/portainer
 
 echo "Script executed successfully."
